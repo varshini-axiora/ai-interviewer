4ae7b177612ff63f13c8837a4ef76a800deffe0e
varshini-axiora <varshini.bobbarala@axioraglobalsolutions.com>
2026-04-13 22:06:37 +0530

    Delete

--- apps/api/src/modules/domains/handlers.js
+++
@@ -1,8 +0,0 @@
-import { sendJson } from '../../common/http.js';
-import { listActiveDomains } from './repository.js';
-
-export async function handleListDomains(_req, res) {
-  const domains = await listActiveDomains();
-  sendJson(res, 200, { ok: true, data: domains });
-}
-
